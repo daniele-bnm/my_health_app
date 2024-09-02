3286240007cf84f7bb85885cf9982f3cd2b1e073
daniele <d.bonometti007@studenti.unibs.it>
2024-09-02 16:03:14 +0200

    added login manager

--- health_app/__init__.py
+++ health_app/__init__.py
@@ -1,19 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 
 bcrypt = Bcrypt()
 db = SQLAlchemy()
+login_manager = LoginManager()
 
-def create_app():
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object('config.Config')
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config.Config')
 
-    db.init_app(app)
-    bcrypt.init_app(app)
+db.init_app(app)
+bcrypt.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
-    from health_app import routes, models
+from health_app import routes
 
-    return app
