03c0e44d84122c3342b1ae065889dbc396e0247f
daniele <d.bonometti007@studenti.unibs.it>
2024-09-02 16:07:28 +0200

    completed the login and logout routes

--- health_app/routes.py
+++ health_app/routes.py
@@ -1,7 +1,7 @@
-from crypt import methods
-
 from flask import render_template, flash, redirect, url_for
-from health_app import app, db, bcrypt
+from flask_login import login_user, logout_user, login_required
+
+from health_app import  app, db
 from health_app.forms import LoginForm, RegistrationForm
 from health_app.models import Consumer
 
@@ -10,11 +10,12 @@ def landing_page():
     return render_template('landing_page.html')
 
 @app.route('/home')
-def home():
+@login_required
+def home_page():
     return render_template('home.html')
 
 @app.route('/register', methods=['GET', 'POST'])
-def register():
+def register_page():
     form = RegistrationForm()
     if form.validate_on_submit():
         # Create a new user with the form data
@@ -27,8 +28,9 @@ def register():
         # Add the user to the database
         db.session.add(user)
         db.session.commit()
+        login_user(user)
         flash(f'Account created!', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('home_page'))
     if form.errors != {}:
         for err_msg in form.errors.values():
             msg = f'{err_msg}'
@@ -37,15 +39,23 @@ def register():
 
 
 @app.route('/login', methods=['GET', 'POST'])
-def login():
+def login_page():
     form=LoginForm()
     if form.validate_on_submit():
         user = Consumer.query.filter_by(Email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.PasswordHash, form.password.data):
-            flash(f'Login Successfull!', 'success')
-        return redirect(url_for('home'))
+        if user and user.check_password_hash(form.password.data):
+                login_user(user)
+                return redirect(url_for('home_page'))
+        else:
+            flash('Invalid email or password', 'warning')
     if form.errors != {}:
         for err_msg in form.errors.values():
             msg = f'{err_msg}'
             flash(f'There was an error: {msg[2:-2]}', 'warning')
     return render_template('auth/login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect(url_for("landing_page"))
