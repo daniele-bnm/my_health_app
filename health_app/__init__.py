860892ca3ca45162422cfe06ff752ae9496e6d55
daniele <d.bonometti007@studenti.unibs.it>
2024-09-02 10:45:30 +0200

    refactor for initialization of db and bcrypt

--- health_app/__init__.py
+++ health_app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
-from health_app.models import db, bcrypt
 
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config.Config')
 
-db.init_app(app)
-bcrypt.init_app(app)
+bcrypt = Bcrypt()
+db = SQLAlchemy()
 
-from health_app import routes, models
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+
+    from health_app import routes, models
+
+    return app
