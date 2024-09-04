1448dec358538c5dc803cee456258b224517667d
daniele <d.bonometti007@studenti.unibs.it>
2024-09-04 11:43:58 +0200

    changed the db reference

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     STATIC_FOLDER = 'health_app/static'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://dan:miaKeyLong@localhost/nextcartdb_dev'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://dan:miaKeyLong@localhost/nextcartdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
