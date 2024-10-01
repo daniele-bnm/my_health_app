e318bac6f25ec1279cf022cc6ca72523b8f423f2
daniele <d.bonometti007@studenti.unibs.it>
2024-10-01 21:35:04 +0200

    Removed my link to the database. Now it's a generic version that needs to be modified by the administrator.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     STATIC_FOLDER = 'health_app/static'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://dan:miaKeyLong@localhost/nextcartdb'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://username:password@localhost/name_of_the_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
 
